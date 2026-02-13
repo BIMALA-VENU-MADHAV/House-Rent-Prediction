370af6ae89d9cb7a40f8b277e4328374cc347272
BIMALA VENU MADHAV <venubimala1234@gmail.com>
2026-02-13 19:30:55 +0530

    Added Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn house_rent_prediction.wsgi
--- house_rent_prediction/settings.py
+++ house_rent_prediction/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'house_rent_prediction.urls'
@@ -120,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets/static'), ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
